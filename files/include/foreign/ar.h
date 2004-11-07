93baa77a7d68e3a01f1cbd3008495662c33970c6
ssen <ssen@a073acdd-5acc-438f-8c47-dd119020c213>
2004-11-07 08:59:30 +0000

    Add a set of faked up mach and other headers from Darwin. These are activated when cross compiling from another OS
    git-svn-id: http://svn.macosforge.org/repository/odcctools/trunk@48 a073acdd-5acc-438f-8c47-dd119020c213

---
+++ files/include/foreign/ar.h
@@ -0,0 +1,20 @@
+#ifndef _AR_H_
+#define _AR_H_
+
+#define ARMAG           "!<arch>\n"     /* ar "magic number" */
+#define SARMAG          8               /* strlen(ARMAG); */
+
+#define AR_EFMT1        "#1/"           /* extended format #1 */
+
+struct ar_hdr {
+  char ar_name[16];               /* name */
+  char ar_date[12];               /* modification time */
+  char ar_uid[6];                 /* user id */
+  char ar_gid[6];                 /* group id */
+  char ar_mode[8];                /* octal file permissions */
+  char ar_size[10];               /* size in bytes */
+#define ARFMAG  "`\n"
+  char ar_fmag[2];                /* consistency check */
+};
+
+#endif /* !_AR_H_ */
